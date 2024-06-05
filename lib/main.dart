ede68990e5fd72aa54d41d877649fcd616f14e6b
Sam <sam.tepec.dev@gmail.com>
2024-06-05 12:19:26 -0600

    Created dialogs

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Widgets Catalog',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 5).getTheme(),
